7538bde4fe11ebcbc63f2bfb77e8f48a7cd8f87e
Michael Busta <busta2356@gmail.com>
2016-06-07 18:15:34 -0400

    added image upload to user model/started profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160607142754) do
+ActiveRecord::Schema.define(version: 20160607215412) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -51,6 +51,10 @@ ActiveRecord::Schema.define(version: 20160607142754) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
