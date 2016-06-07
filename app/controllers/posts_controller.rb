69cf8222753cda16189f4351f03c2f5e484a7bee
Michael Busta <busta2356@gmail.com>
2016-06-07 08:50:24 -0400

    set up paperclip for image uploading

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,52 +1,64 @@
 class PostsController < ApplicationController
-  before_action :find_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+	before_action :find_post, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
+	before_action :authenticate_user!, except: [:index, :show]
 
-  def index
-    @posts = Post.all.order("created_at DESC")
-  end
+	def index
+		@posts = Post.all.order("created_at DESC")
+	end
 
-  def show
-  end
+	def show
+		# @comments = Comment.where(post_id: @post)
+		@random_post = Post.where.not(id: @post).order("RANDOM()").first
 
-  def new
-    @post = current_user.post.build
-  end
+	end
 
-  def create
-    @post = current_user.post.build(post_params)
+	def new
+		@post = current_user.posts.build
+	end
 
-    if @post.save
-      redirect_to @post
-    else
-      render 'new'
-    end
-  end
+	def create
+		@post = current_user.posts.build(post_params)
 
-  def edit
-  end
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
 
-  def update
-    if @post.update(post_params)
-      redirect_to @post
-    else
-      render 'edit'
-    end
-  end
+	def edit
+	end
 
-  def destroy
-    @post.destroy
-    redirect_to root_path
-  end
+	def update
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
 
-  private
+	def destroy
+		@post.destroy
+		redirect_to root_path
+	end
 
-  def find_post
-    @post = Post.find(params[:id])
-  end
+	def upvote
+		@post.upvote_by current_user
+		redirect_to :back
+	end
 
-  def post_params
-    params.require(:post).permit(:title, :link, :description)
-  end
+	def downvote
+		@post.downvote_from current_user
+		redirect_to :back
+	end
 
+	private
+
+	def find_post
+		@post = Post.find(params[:id])
+	end
+
+	def post_params
+		params.require(:post).permit(:title, :link, :description, :image)
+	end
 end
