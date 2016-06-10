00a300669adf0e805e26107a10735ceb78be220b
Michael Busta <busta2356@gmail.com>
2016-06-09 22:00:27 -0400

    user can succesfully submit comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:post_id])
     @comment = Comment.create(params[:comment].permit(:content))
     @comment.user_id = current_user.id
     @comment.post_id = @post.id
